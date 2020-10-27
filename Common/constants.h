5dd9d68680f4032c3768208418093bd7da49d155
John Dacquisto <johndacquisto1@gmail.com>
2020-10-26 21:51:19 -0400

    Miscellaneous cleanup.

--- Common/constants.h
+++ Common/constants.h
@@ -79,9 +79,9 @@
 
 #define SPEED_OF_LIGHT_FEET_PER_SECOND 9.8425e8
 
-// JAD TODO: Not sure the significance of this value. May have to do with going from ERP watts
+// Not sure the significance of this value. May have to do with going from ERP watts
 // to field strength volts, and/or going from frequency to meters via speed-of-light?
 #define FIELD_STRENGTH_MAGIC_NUMBER 139.4
 
-// JAD TODO: Not sure the significance of this value.
+// Not sure the significance of this value.
 #define POWER_DENSITY_MAGIC_NUMBER 2589988.11
--- SplatLib/SplatLib.cpp
+++ SplatLib/SplatLib.cpp
@@ -1191,7 +1191,6 @@ ReadLongleyRiceParameterDataForSite(
 	itmParameters->effectiveRadiatedPower = 0.0;
 
 	// Generate .lrp filename from txsite filename.
-	// JAD TODO: Change this approach to accomodate relative file paths.
 
 	for (x = 0; (txsite.filename[x] != '.') && (txsite.filename[x] != 0) && (x < 250); x++)
 	{
